0df42934a2245522f5596bd5b241d267bd1aed0f
Tariqul Islam <cavidpark@gmail.com>
2021-06-02 12:07:44 +0600

    Added dictionary mapping

---
+++ Movie_ChatBot.py
@@ -0,0 +1,98 @@
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+# Import Dataset
+lines = open('movie_lines.txt', encoding = 'utf8', errors='ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding='utf8', errors='ignore').read().split('\n')
+
+# Creating a dictionary that maps each line and its ID
+
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+        
+# creating a list of all the conversations
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+
+ 
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+        
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", "will", text)
+    text = re.sub(r"\'ve", "have", text)
+    text = re.sub(r"\'re", "are", text)
+    text = re.sub(r"\'d", "would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=|.?,]", "", text)
+    return text
+
+
+# CLeaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+    
+# cleaning the answers    
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+
+# Creating a dictonary that maps each word to its number of occurences
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+            
+            
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] +=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              
+                  
