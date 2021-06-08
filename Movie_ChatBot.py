5d60150f9207f9f1ff1f38f663e11d3bb880187d
Tariqul Islam <cavidpark@gmail.com>
2021-06-08 20:16:19 +0600

    added Decoder RNN

--- Movie_ChatBot.py
+++ Movie_ChatBot.py
@@ -166,7 +166,7 @@ def preprocess_targets(targets, word2int, batch_size):
 
 # Creating the Encoder RNN Layer
 
-def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+def encoder_rnn(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
     lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
     lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob=keep_prob)
     encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
@@ -209,20 +209,57 @@ def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix, sos_
                                                                               attention_values,
                                                                               attention_score_function,
                                                                               attention_construct_function,
-                                                                              name = "attn_dec_train")
-    decoder_output, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
-                                                                                                              test_decoder_function,
-                                                                                                              decoder_embeddings_matrix,
-                                                                                                              sequence_length,
-                                                                                                              scope = decoding_scope)
-    decoder_output_dropout = tf.nn.dropout(decoder_output, keep_prob)
-    return output_function(decoder_output_dropout)
+                                                                              decoder_embeddings_matrix,
+                                                                              sos_id, 
+                                                                              eos_id, 
+                                                                              maximum_length, 
+                                                                              num_words,
+                                                                              name = "attn_dec_inf")
+    test_predictions, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
+                                                                                                                test_decoder_function,                                                                                                            
+                                                                                                                scope = decoding_scope)
+    return test_predictions
 
 
 
+# Create the Decoder RNN
 
 
-
+def decoder_rnn(decoder_embedded_input, decoder_embeddings_matrix, encoder_state, num_words, sequence_length, rnn_size, num_layers, word2int, keep_prob, batch_size):
+    with tf.variable_scope("decoding") as decoding_scope:
+        lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+        lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = keep_prob)
+        decoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+        weights = tf.truncated_normal_initializer(stddev=0.1)
+        biases = tf.zeros_initializer()
+        output_function = lambda x: tf.contrib.layers.fully_connected(x,
+                                                                      num_words,
+                                                                      None,
+                                                                      scope=decoding_scope,
+                                                                      weights_initializer=weights,
+                                                                      biases_initializer=biases)
+        training_predictions = decode_training_set(encoder_state,
+                                                   decoder_cell,
+                                                   decoder_embedded_input,
+                                                   sequence_length,
+                                                   decoding_scope,
+                                                   output_function,
+                                                   keep_prob,
+                                                   batch_size)
+        decoding_scope.reuse_variables()
+        test_predictions = decode_test_set(encoder_state,
+                                           decoder_cell,
+                                           decoder_embeddings_matrix,
+                                           word2int['<SOS>'],
+                                           word2int['<EOS>'],
+                                           sequence_length -1,
+                                           num_words,
+                                           decoding_scope,
+                                           output_function,
+                                           keep_prob,
+                                           batch_size)
+    return training_predictions, test_predictions        
+        
 
 
 
